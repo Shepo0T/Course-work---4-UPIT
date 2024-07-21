7e6b2ae5f64fce529e7ba9d5c3c4be1a661d5bf3
Aleksandr <zemacohoznik@gmail.com>
2024-07-21 13:13:53 +0300

    создание абстрактного класса для работы с Json

--- src/Abstraction.py
+++ src/Abstraction.py
@@ -2,8 +2,23 @@ from abc import ABC, abstractmethod
 
 class HH_ApiAbstract(ABC):
     @abstractmethod
-    def get_response(self):
+    def get_response(self, keyword, per_page):
         pass
     @abstractmethod
+    def get_vacancies(self, keyword, per_page):
+        pass
+
+
+class Creat_Json(ABC):
+
+    @abstractmethod
+    def write(self, vacancies):
+        pass
+
+    @abstractmethod
     def get_vacancies(self):
+        pass
+
+    @abstractmethod
+    def delete_vacancies(self):
         pass
