2e9cf3a204e77f26d5e7ccfaa3b89b20c7c5e3de
Aleksandr <zemacohoznik@gmail.com>
2024-07-21 16:10:02 +0300

    окончательный вид класса HH

--- src/HH_Api.py
+++ src/HH_Api.py
@@ -12,12 +12,12 @@ class HH(HH_ApiAbstract):
         self.url = 'https://api.hh.ru/vacancies'
         self.headers = {'User-Agent': 'HH-User-Agent'}
         self.vacancies = []
-        self.params = {"text": "", "per_page": ""}
+        self.params = {"text":"", "per_page": ""}
 
     def get_response(self, keyword: str, per_page: int ) -> Response:
         self.params['text'] = keyword
         self.params['per_page'] = per_page
-        return requests.get(self.url, params=self.params)
+        return requests.get(url=self.url, params=self.params)
 
     def get_vacancies(self, keyword: str, per_page: int):
         return self.get_response(keyword, per_page).json()['items']
