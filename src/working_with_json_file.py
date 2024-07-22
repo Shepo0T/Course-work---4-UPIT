5214b0214cdd14fdb974d1a5a031d9a180964cc6
Aleksandr <zemacohoznik@gmail.com>
2024-07-22 20:34:01 +0300

    редактирование класса Json, не создавал файл в папке data

--- src/working_with_json_file.py
+++ src/working_with_json_file.py
@@ -1,14 +1,20 @@
 import json
+
 from src.work_with_vacancies import Vacancies
+
 from src.Abstraction import Creat_JsonAbstract
+
+
 class CreationJson(Creat_JsonAbstract):
     """Класс для создания json файла"""
-    def __init__(self, filename='../data/vacancies.json'):
-        self.filename = filename
+
+    def __init__(self, filename='vacancies.json'):
+        self.filename = f'data/{filename}'
 
     def write(self, vacancies):
         with open(self.filename, 'w') as file:
             json.dump(vacancies, file, ensure_ascii=False, indent=4)
+
     def get_vacancies(self):
         with open(self.filename, 'r') as file:
             data = json.load(file)
